73fb272e1daca6b43bbd2ef43000d6fdd34747eb
Biswas <biswasb007@gmail.com>
2017-03-06 13:51:51 +0530

    removed unneccessary pics and optimised libs and jpgs

--- config/settings/production.py
+++ config/settings/production.py
@@ -49,7 +49,7 @@ MIDDLEWARE = RAVEN_MIDDLEWARE + MIDDLEWARE
 # and https://docs.djangoproject.com/ja/1.9/howto/deployment/checklist/#run-manage-py-check-deploy
 
 # set this to 60 seconds and then to 518400 when you can prove it works
-#Commenting for releaving vsrf
+#Commenting for relieving csrf
 """
 SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
