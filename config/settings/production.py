f9199d208cc77cdda1ded6d238d6af3d7d1938a0
Biswas <biswasb007@gmail.com>
2017-03-06 11:25:27 +0530

    admin url in production setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -196,3 +196,5 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
+
+ADMIN_URL = r'^admin/'
