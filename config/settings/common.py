8905376f2a198bc761644250b09c888bc78d99c8
Biswas <biswasb007@gmail.com>
2017-03-08 17:40:20 +0530

    Debug false

--- config/settings/common.py
+++ config/settings/common.py
@@ -256,5 +256,3 @@ ADMIN_URL = r'^admin/'
 
 # Your common stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
-#TODO: WARNING: Remove this
-DEBUG = True
