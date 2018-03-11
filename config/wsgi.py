e712522f67e7561c34cade8c1b95477a0f9f6a25
Biswas B <Biswasb007@gmail.com>
2018-03-11 07:37:00 +0530

    fix sentry import

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,10 +16,10 @@ framework.
 import os
 
 from django.core.wsgi import get_wsgi_application
-"""
+
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
     from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-"""
+
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
